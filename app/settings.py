0efaf4994f7af3c1c7eb09841069d283300bd94b
shelest2024 <158695731+shelest2024@users.noreply.github.com>
2024-02-03 13:19:12 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -156,6 +156,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Добавление медиа
 MEDIA_URL = "media/"
 
