cc6071061c1145d25241622a83c7ebc0d2b22e42
shelest2024 <158695731+shelest2024@users.noreply.github.com>
2024-02-03 10:14:25 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from app import settings
 
@@ -12,6 +13,8 @@ urlpatterns = [
     path("cart/", include('carts.urls', namespace='cart')),
     path("orders/", include('orders.urls', namespace='orders')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 # Отладка в дебаг режими
 if settings.DEBUG:
