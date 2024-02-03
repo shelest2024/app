41186de97fecf44f343c9a7158475f68e1e1d8d0
shelest2024 <158695731+shelest2024@users.noreply.github.com>
2024-02-03 11:30:17 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -151,13 +151,12 @@ INTERNAL_IPS = [
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    'static/'
+    BASE_DIR / "static",
 ]
-
 # Добавление медиа
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
